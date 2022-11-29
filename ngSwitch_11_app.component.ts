ffeced6ec1b92a6d4f69987e5247ddcb25579c95
The DotNet Office <muneshs16@gmail.com>
2022-11-29 16:54:11 +0530

    Add files via upload

---
+++ ngSwitch_11_app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular 15 App';
+
+  public color = '';
+
+  SetColor(colorVal:any)
+  {
+   this.color = colorVal.target.value;
+  }
+}
