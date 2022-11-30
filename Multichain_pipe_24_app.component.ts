0340f81f5218a73194ffeef91c5c7ca0a2387f8f
The DotNet Office <muneshs16@gmail.com>
2022-11-30 15:52:02 +0530

    Multichain pipe

---
+++ Multichain_pipe_24_app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular 15 App';
+
+  DOB = new Date('01/05/1990');
+
+  Salary = 1000000;
+
+
+  constructor(){
+
+  
+  }
+
+}
