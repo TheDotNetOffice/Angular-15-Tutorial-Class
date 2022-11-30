26426eaefa91ab997b082e6695c460d33c89d8ba
The DotNet Office <muneshs16@gmail.com>
2022-11-30 14:43:58 +0530

    Two way data binding

---
+++ TwoWayBinding_21_app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular 15 App';
+
+ name1 = "Dotnet Office-1";
+  
+ name2 = "Dotnet";
+}
