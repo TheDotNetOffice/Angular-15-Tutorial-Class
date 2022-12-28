bc1a998b1421864e0d387dfabf184a0b8added42
The DotNet Office <muneshs16@gmail.com>
2022-12-28 19:05:40 +0530

    Add files via upload

---
+++ Passing Query Parameters to Route/student.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Angular15Service } from '../angular15.service';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent {
+  Studentinfo: any = [];
+  constructor(private service: Angular15Service) {
+    console.log("StudentComponent");
+  }
+
+  ngOnInit() {
+
+    this.service.GetStudentDetails().subscribe(data=>{
+      this.Studentinfo = data;
+    })
+    
+  }
+
+}
