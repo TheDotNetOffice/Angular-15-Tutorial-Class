90d777480ffedc75b299c7ee0912608d3a280d61
The DotNet Office <muneshs16@gmail.com>
2022-12-28 20:43:43 +0530

    Add files via upload

---
+++ Passing Fragments to Route/app.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { observable, Observable, reduce, of, from } from 'rxjs';
+import { StudentStandAloneComponent } from './student-stand-alone/student-stand-alone.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  //standalone:true,
+  //imports:[CommonModule,RouterModule]
+})
+export class AppComponent {
+  title = 'angular 15 App';
+
+  constructor(private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.activatedRoute.fragment.subscribe((section) => {
+
+      this.goToSection(section);
+    })
+  }
+
+  goToSection(section: any = "") {
+    document.getElementById(section)!.scrollIntoView({ behavior: 'smooth' });
+
+  }
+
+}
